c2132ec15d136bd2555ac24693ba42bca88b251d
gal-legit <gal@legitsecurity.com>
2022-10-18 16:13:44 +0300

    remote attest

--- cmd/legit_remote_attest/main.go
+++ cmd/legit_remote_attest/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"log"
 
 	"github.com/legit-labs/legit-attestation/pkg/legit_remote_attest"
 )
@@ -17,6 +18,14 @@ func main() {
 	flag.StringVar(&endpoint.ApiToken, "api-token", "", "The api-token to Legit")
 
 	flag.Parse()
+
+	if subjectsBase64 == "" {
+		log.Panicf("missing subjects")
+	} else if endpoint.Url == "" {
+		log.Panicf("missing url")
+	} else if endpoint.ApiToken == "" {
+		log.Panicf("missing api token")
+	}
 
 	legit_remote_attest.Attest(subjectsBase64, endpoint)
 }
